53eeed8e5ce839c55e8af3bd4daaac3aad98c45a
Livshitz <eli.livshitz@gmail.com>
2014-11-12 20:25:32 +0200

    + LogDispalyer

--- Liv.Log.Tests/Form1.Designer.cs
+++ Liv.Log.Tests/Form1.Designer.cs
@@ -28,12 +28,33 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.logDisplayer1 = new Liv.Log.LogDisplayer();
+			this.SuspendLayout();
+			// 
+			// logDisplayer1
+			// 
+			this.logDisplayer1.Location = new System.Drawing.Point(13, 13);
+			this.logDisplayer1.Name = "logDisplayer1";
+			this.logDisplayer1.Size = new System.Drawing.Size(525, 275);
+			this.logDisplayer1.TabIndex = 0;
+			this.logDisplayer1.TraceFile = ".\\Trace\\Log.log";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(550, 300);
+			this.Controls.Add(this.logDisplayer1);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private LogDisplayer logDisplayer1;
 	}
 }
 
--- Liv.Log.Tests/Program.cs
+++ Liv.Log.Tests/Program.cs
@@ -12,46 +12,12 @@ namespace Liv.Log.Tests
 		[STAThread]
 		static void Main()
 		{
-			#region Log usage demo
-			// Create console window and auto listen to logger
-			Log.SetConsoleTracing();
-			// Or manually add to listeners
-			/*
-			var ctl = new ConsoleTraceListener();
-			ctl.Filter = new EventTypeFilter(SourceLevels.Verbose);
-			Trace.Listeners.Add(ctl);
-			Trace.AutoFlush = true;
-			*/
-
-			// Set write log into file
-			Log.SetWriteToFile("myLog.log");
-
-			Log.Info("Hello there!");
-			Log.Verbose("This is verbose, and look at this {0} & fast way to write to log", "awsome");
-			Log.SetLogLevel(Log.TraceLevel.Info);
-			Log.Verbose("This you should not see");
-			Log.SetLogLevel(Log.TraceLevel.Debug);
-			Log.Debug(Compunentes.CompenentA, "This is debug message for CompenentA");
-			Log.TraceEntity(new MyCustomClass() { Title = "This is the title", Number = 5 });
-			#endregion
-
-
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new Form1());
 		}
 
-		enum Compunentes
-		{
-			CompenentA,
-			CompenentB,
-		}
-
-		class MyCustomClass
-		{
-			public string Title { get; set; }
-			public int Number { get; set; }
-		}
+		
 
 	}
 }
